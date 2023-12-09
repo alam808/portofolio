f0b78b6bcc944df8c05d6a4f3bb902cdae663ed2
Alam0x <91639995+alam808@users.noreply.github.com>
2023-12-09 10:07:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Social Media Specialist", "Community Manager", "Digital Marketing", "Copywritter", "Ai Prompt"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
